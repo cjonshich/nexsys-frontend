85a15750472e0b26a595f24ce5fb7bad9ee9b98b
Cristian Bolije <cbolije@gmail.com>
2019-07-13 13:15:09 -0500

    node + express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+// Config
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+
+// Init
 app.listen(3000, () => {
-    console.log('Server on port 3000');
+    console.log('Server on port ', app.get('port'));
 })
